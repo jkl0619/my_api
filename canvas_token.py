aea949061e1792449bff59e8344856f8af277b05
h90n <36554402+h90n@users.noreply.github.com>
2018-04-22 19:16:45 -0400

    Update canvas_token.py

--- canvas_token.py
+++ canvas_token.py
@@ -1,2 +1,10 @@
-#Canvas token - Hao
-4511~c0RQ8ID0VbeODWXG5A9TIty3C0yjRDgmrRMx6LynhuYeQrPh9suqVmTSERprZTrC
+"""
+This file will be used to store your canvas token separately from your server code.
+
+DO NOT change the variable name.
+
+Change the value to a Canvas Token that you generated yourself.
+
+"""
+
+canvas_token = "4511~c0RQ8ID0VbeODWXG5A9TIty3C0yjRDgmrRMx6LynhuYeQrPh9suqVmTSERprZTrC"
