22d77f308f4877ec074fa7c8eeb843af2c4bd260
Jae ho Lee <jae0619@vt.edu>
2018-04-22 05:14:03 -0400

    Big junmble of mess rn

---
+++ mongodb_setup.py
@@ -0,0 +1,18 @@
+import pymongo
+
+
+
+connection = pymongo.Connection()
+
+db = connection["User_Info"]
+accounts = db["accounts"]
+
+accounts.insert({"username": "admin", 'password':'pass'})
+accounts.insert({"username": "chris", 'password':'hello'})
+accounts.insert({"username": "hao", 'password':'world'})
+accounts.insert({"username": "jae", 'password':'gdi'})
+
+#To look up the account
+#cursor = db.accounts.find()
+#accounts.find({"username":parameterHere})[0]
+
