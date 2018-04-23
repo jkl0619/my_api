dd5aae993c6c0dc9cf23b6ad496d9d7296e42083
h90n <36554402+h90n@users.noreply.github.com>
2018-04-23 15:46:16 +0000

    Update led.py

--- led.py
+++ led.py
@@ -1,4 +1,5 @@
-import led_pins
+#import led_pins
+import json
 import time
 #import RPi.GPIO as GPIO
 from flask import Flask, request, redirect, jsonify
@@ -22,69 +23,78 @@ app = Flask(__name__)
 
 	
 led_data ={
-		'red':0.0,
-		'green':0.0,
-		'blue':0.0,
-		'rate':0.0,
-		'state':0
+    'red':0.0,
+    'green':0.0,
+    'blue':0.0,
+    'rate':0.0,
+    'state':0
 }
-
-@app.route("/poop", methods =['GET'])
+ 
+@app.route("/led", methods =['GET'])
 def led_get():
 	retVal = json.dumps(led_data)
-	print(retVal)
+	return retVal
 
-# @app.route("/led/<path:varargs>", methods = ['POST'])
-# def led(varargs = None):
-	
-	# varargs = varargs.split("/")
-	
-	# if varargs.length()%2 == 0:
-		# for param in range(0, varargs.length(), 2):
-			# print(str(param))
-	# else:
-		# print("Error: Failed to set values.")
+@app.route("/led/<path:varargs>", methods = ['POST'])
+def led(varargs=None):
+
+    listarg = varargs.split("/")
+    count = 0        
+    if len(varargs)%2 == 1:
+        for count in range(len(listarg)):
+            if listarg[count] == 'red':
+                led_data['red'] = float(listarg[count+1])
+            if listarg[count] == 'green':
+                led_data['green'] = float(listarg[count+1])
+            if listarg[count] == 'blue':
+                led_data['blue'] = float(listarg[count+1])
+            if listarg[count] == 'rate':
+                led_data['rate'] = float(listarg[count+1])
+            if listarg[count] == 'state':
+                led_data['state'] = int(listarg[count+1])
+        successstr = "Success! New data values set \n"
+    else:
+        return "Failure to set new data values."
+    
+    return json.dumps(led_data) +"\n" + json.dumps(listarg) + "\n"
+        #eturn successstr
+#    else:
+#        errorstr = "Error: Failed to set values. \n"
+#        return errorstr
 		
-	
-@app.route("/led/red/<int:v1>", methods = ['POST'])
-def led(v1):
-	
-	print(v1)
-		
-
-
 try:
-	while led_data['state'] == 1:
-		for dcr in range(0, led_data['red']+1, 1):
-#            r.ChangeDutyCycle(dc)
-			print(str(dcr))
-			time.sleep(rate)
-			
-		for dcr in range(led_data['red'], -1, -1):
-#           r.ChangeDutyCycle(dc)
-			print(str(dcr))
-			time.sleep(rate)
+    while led_data['state'] == 1:
+        print("poop")
+        for dcr in range(0, led_data['red']+1, 1):
+            #r.ChangeDutyCycle(dc)
+            print(str(dcr))
+            time.sleep(rate)
+                
+        for dcr in range(led_data['red'], -1, -1):
+        #           r.ChangeDutyCycle(dc)
+            print(str(dcr))
+            time.sleep(rate)
 
-			
-		for dcg in range(0, led_data['green']+1, 1):
-#            r.ChangeDutyCycle(dc)
-			print(str(dcg))
-			time.sleep(rate)
-			
-		for dcg in range(led_data['green'], -1, -1):
-#           r.ChangeDutyCycle(dc)
-			print(str(dcg))
-			time.sleep(rate)
-			
-		for dcb in range(0, led_data['blue']+1, 1):
-#            r.ChangeDutyCycle(dc)
-			print(str(dcb))
-			time.sleep(rate)
-			
-		for dcb in range(led_data['blue'], -1, -1):
-#           r.ChangeDutyCycle(dc)
-			print(str(dcb))
-			time.sleep(rate)
+                
+        for dcg in range(0, led_data['green']+1, 1):
+        #            r.ChangeDutyCycle(dc)
+            print(str(dcg))
+            time.sleep(rate)
+                
+        for dcg in range(led_data['green'], -1, -1):
+        #           r.ChangeDutyCycle(dc)
+            print(str(dcg))
+            time.sleep(rate)
+                
+        for dcb in range(0, led_data['blue']+1, 1):
+        #            r.ChangeDutyCycle(dc)
+            print(str(dcb))
+            time.sleep(rate)
+                
+        for dcb in range(led_data['blue'], -1, -1):
+        #           r.ChangeDutyCycle(dc)
+            print(str(dcb))
+            time.sleep(rate)
 	
 except KeyboardInterrupt:
 	pass
@@ -98,3 +108,4 @@ except KeyboardInterrupt:
 if __name__ == "__main__":
 	app.run(host='127.0.0.1', port = 5555, debug=True)		
 	
+
