60e0aea19efac281702403d2307330c7228a76c5
Jae ho Lee <jae0619@vt.edu>
2018-04-22 23:36:02 -0400

    Fully functioning client excluding zeronconf

--- custom.py
+++ custom.py
@@ -3,6 +3,7 @@ import os
 import time
 from twitter import *
 from flask import Flask, request, render_template, redirect, abort, flash, jsonify
+from clientKeys import *
 
 address = ""
 
@@ -19,13 +20,13 @@ class MyListener(object):
 
 print(address)
 
-zeroconf = Zeroconf()
-listener = MyListener()
-browser = ServiceBrowser(zeroconf, "_http._tcp.local.", listener)
-try:
-    input("Press enter to exit...\n\n")
-finally:
-    zeroconf.close()
+#zeroconf = Zeroconf()
+#listener = MyListener()
+#browser = ServiceBrowser(zeroconf, "_http._tcp.local.", listener)
+#try:
+#    input("Press enter to exit...\n\n")
+#finally:
+#    zeroconf.close()
 
 #curl -H "Authorization: Bearer <ACCESS-TOKEN>" "https://canvas.instructure.com/api/v1/courses"
 
@@ -37,84 +38,52 @@ finally:
 
 # coding: utf-8
 
-app = Flask(__name__)  # create our flask app
+app = Flask(__name__)   # create our flask app
 
 # configure Twitter API
-twitter = Twitter(
-    auth=OAuth(os.environ.get('OAUTH_TOKEN'), os.environ.get('OAUTH_SECRET'),
-               os.environ.get('CONSUMER_KEY'), os.environ.get('CONSUMER_SECRET'))
-
-)
+twitter = Twitter(auth=OAuth(access_token, access_token_secret, consumer_key, consumer_secret))
 
 
-@app.route('/')
-def main():
-    # fetch 3 tweets from my account
-    myTweets = twitter.statuses.user_timeline(count=10)
-
-    # fetch 3 tweets from ITP_NYU
-    itpTweets = twitter.statuses.user_timeline(screen_name='itp_nyu', count=10)
-
-    # app.logger.debug(itpTweets)
-
-    templateData = {
-        'title': 'My last three tweets',
-        'myTweets': myTweets,
-        'itpTweets': itpTweets
-    }
-
-    return render_template('index.html', **templateData)
+# configure Twitter API
+# set consumer token + access token
 
 
-@app.route('/search')
-def search():
-    # get search term from querystring 'q'
-    query = request.args.get('q', '#redburns')
+#Use %20 for space in Curl, especially for messages
+#updates the status with the specified message
+@app.route('/status/<string:message>', methods = ['POST'])
+def statusUpdate(message):
+    twitter.statuses.update(status=message)
+    updateMessage= "Updated the status saying: " + message
+    return updateMessage
 
-    # search with query term and return 10
-    results = twitter.search.tweets(q=query, count=50)
-
-    # return jsonify(results)
-    # app.logger.debug(results)
-
-    templateData = {
-        'query': query,
-        'tweets': results.get('statuses')
-    }
-
-    return render_template('search.html', **templateData)
-
-
-@app.route('/post', methods=['GET', 'POST'])
-def post_to_twitter():
-    if request.method == 'POST':
-        result = twitter.statuses.update(status=request.form.get('status'))
-
-        app.logger.debug(result)
-
-        # redirect to new twitter status post
-        return redirect('http://www.twitter.com/%s/status/%s' % (result['user']['screen_name'], result.get('id')))
-
-    else:
-        return render_template('post_to_twitter.html')
+#Sends a message to the user specified
+@app.route('/dm/<string:username>/<string:message>', methods = ['POST'])
+def sendDM(username,message):
+    twitter.direct_messages.new(
+        user=username,
+        text=message)
+    updateMessage = "Sent the DM to " + username
+    updateMessage = updateMessage + "with the message: " + message
+    return updateMessage
 
 
-@app.errorhandler(404)
-def page_not_found(error):
-    return render_template('404.html'), 404
+@app.route('/firstTweet', methods=['GET'])
+def first_Tweet():
+    x = twitter.statuses.home_timeline()
 
+    # The username of the first tweet on your timeline
+    return x[0]['user']['screen_name']
 
-# This is a jinja custom filter
-@app.template_filter('strftime')
-def _jinja2_filter_datetime(date, fmt=None):
-    pyDate = time.strptime(date, '%a %b %d %H:%M:%S +0000 %Y')  # convert twitter date string into python date/time
-    return time.strftime('%Y-%m-%d %H:%M:%S', pyDate)  # return the formatted date.
+@app.route('/firstTweet/friend/<string:username>', methods=['GET'])
+def first_Tweet_Friend(username):
+    x = twitter.statuses.user_timeline(screen_name=username)
+
+    # The username of the first tweet on their timeline
+    return x[0]['user']['screen_name']
 
 
 # --------- Server On ----------
 # start the webserver
 if __name__ == "__main__":
     app.debug = True
-
-    port = int(os.environ.get('PORT', 5000))  # locally PORT 5000, Heroku will assign its own port
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=8090)
