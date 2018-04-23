b6660702718a4ad211968b6b26bcc296cc2000f0
millerc3 <millerc3@vt.edu>
2018-04-23 06:08:50 +0000

    Update mongodb_setup.py

--- mongodb_setup.py
+++ mongodb_setup.py
@@ -16,3 +16,22 @@ accounts.insert({"username": "jae", 'password':'gdi'})
 #cursor = db.accounts.find()
 #accounts.find({"username":parameterHere})[0]
 
+----------------------------------------------------------------------
+
+from pymongo import MongoClient
+
+client = MongoClient()
+
+db = client["User_Info"]
+accounts = db["accounts"]
+
+accounts.insert({"username": "admin", 'password':'pass'})
+accounts.insert({"username": "chris", 'password':'hello'})
+accounts.insert({"username": "hao", 'password':'world'})
+accounts.insert({"username": "jae", 'password':'gdi'})
+
+#To look up the account
+#cursor = db.accounts.find()
+#accounts.find({"username":parameterHere})[0]
+
+
