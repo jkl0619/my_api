d6f4456a1eb84f29be4b415dcf038a044e92dcd0
millerc3 <millerc3@vt.edu>
2018-04-24 03:41:18 +0000

    Update mongodb_setup.py

--- mongodb_setup.py
+++ mongodb_setup.py
@@ -1,37 +1,16 @@
-import pymongo
-
-
-
-connection = pymongo.Connection()
-
-db = connection["User_Info"]
-accounts = db["accounts"]
-
-accounts.insert({"username": "admin", 'password':'pass'})
-accounts.insert({"username": "chris", 'password':'hello'})
-accounts.insert({"username": "hao", 'password':'world'})
-accounts.insert({"username": "jae", 'password':'gdi'})
-
-#To look up the account
-#cursor = db.accounts.find()
-#accounts.find({"username":parameterHere})[0]
-
-----------------------------------------------------------------------
-
 from pymongo import MongoClient
 
 client = MongoClient()
 
-db = client["User_Info"]
-accounts = db["accounts"]
+db = client["database"]
+accounts = db["users"]
 
-accounts.insert({"username": "admin", 'password':'pass'})
-accounts.insert({"username": "chris", 'password':'hello'})
-accounts.insert({"username": "hao", 'password':'world'})
-accounts.insert({"username": "jae", 'password':'gdi'})
+accounts.insert_one({"username": "admin", 'password':'pass'})
+accounts.insert_one({"username": "chris", 'password':'hello'})
+accounts.insert_one({"username": "hao", 'password':'world'})
+accounts.insert_one({"username": "jae", 'password':'gdi'})
 
 #To look up the account
 #cursor = db.accounts.find()
 #accounts.find({"username":parameterHere})[0]
-
 
