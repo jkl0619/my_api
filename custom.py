04e378b3db1e1d35d9b20b75453de916658898c9
Jae ho Lee <jae0619@vt.edu>
2018-04-23 21:28:20 -0400

    refactored code

--- custom.py
+++ custom.py
@@ -8,7 +8,11 @@ import socket
 import logging
 import sys
 
-ownAddress = socket.gethostbyname(socket.gethostname())
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(('8.8.8.8', 1))  # connect() for UDP doesn't send packets
+ownAddress = s.getsockname()[0]
+#ownAddress = socket.gethostbyname(socket.gethostname())
 
 
 if __name__ == '__main__':
@@ -29,61 +33,27 @@ if __name__ == '__main__':
 
 print("zeroconf register success!")
 
-class MyListener(object):
-    def remove_service(self, zeroconf, type, name):
-        print("Service %s removed" % (name,))
-
-    def add_service(self, zeroconf, type, name):
-        info = zeroconf.get_service_info(type, name)
-        # print name, info.get_name(), info.server,
-        print (name)
-        print (info)
-        print (info.server)
-
-
-#zeroconf = Zeroconf()
-#listener = MyListener()
-#browser = ServiceBrowser(zeroconf, "_http._tcp.local.", listener)
-
-#try:
-#    input("Press enter to exit...\n\n")
-#finally:
-#    zeroconf.close()
-
-
-
-# coding: utf-8
-
 app = Flask(__name__)   # create our flask app
 auth = HTTPBasicAuth()
 
 # configure Twitter API
 twitter = Twitter(auth=OAuth(access_token, access_token_secret, consumer_key, consumer_secret))
 
-users = {
-    "admin": "pass",
-    "jae": "laroca69"
-}
-
-@auth.get_password
-def get_pw(username):
-    if username in users:
-        return users.get(username)
-    return None
-
 #Use %20 for space in Curl, especially for messages
 #updates the status with the specified message
-@app.route('/status/<string:message>', methods = ['POST'])
-@auth.login_required
-def statusUpdate(message):
-    twitter.statuses.update(status=message)
-    updateMessage= "Updated the status saying: " + message
+#@app.route('/status/<string:message>', methods = ['POST'])
+@app.route('/status', methods = ['POST'])
+def statusUpdate():
+    f = request.form['message']
+    twitter.statuses.update(status=f)
+    updateMessage = "Updated the status saying: " + f
     return updateMessage
 
 #Sends a message to the user specified
-@app.route('/dm/<string:username>/<string:message>', methods = ['POST'])
-@auth.login_required
-def sendDM(username,message):
+@app.route('/dm>', methods = ['POST'])
+def sendDM():
+    username = request.form['username']
+    message = request.form['message']
     twitter.direct_messages.new(
         user=username,
         text=message)
@@ -93,16 +63,15 @@ def sendDM(username,message):
 
 
 @app.route('/firsttweetuser', methods=['GET'])
-@auth.login_required
 def first_Tweet():
     x = twitter.statuses.home_timeline()
 
     # The username of the first tweet on your timeline
     return x[0]['user']['screen_name']
 
-@app.route('/firsttweetuser/friend/<string:username>', methods=['GET'])
-@auth.login_required
-def first_Tweet_Friend(username):
+@app.route('/firsttweetuser/friend', methods=['GET'])
+def first_Tweet_Friend():
+    username = request.form['username']
     x = twitter.statuses.user_timeline(screen_name=username)
 
     # The username of the first tweet on their timeline
