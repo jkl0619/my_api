ace0dfb3ede6152f3680f6a6ee980343763db0c1
jkl0619 <31525024+jkl0619@users.noreply.github.com>
2018-04-25 20:48:24 -0400

    Update client_keys_twitter.py

--- client_keys_twitter.py
+++ client_keys_twitter.py
@@ -1,7 +1,7 @@
 
 # For Twitter API
 
-consumer_key = "5iDHRUTXZhcQwtDoVtHbvvEM1"
-consumer_secret = "nwqn4dJX0PWSpFD7XJqUJquTnaSeaEovWm9bfndter37alEkaA"
-access_token = "958877956448940032-OehfgltlMRlkG9k7zJeYPhtNmmbySZH"
-access_token_secret = "l0OKIlIzhelQoY90nx4nSIEh1e1AGN6F2HEbhYqpNNots"
+consumer_key = "consumer key here"
+consumer_secret = "consumer secret key here"
+access_token = "access  key here"
+access_token_secret = "access secret key here"
